4709ded838e8b337c2ccc97df73b8957fda20bad
Vítor Lobo <vitorlobof@gmail.com>
2023-08-03 05:48:44 -0300

    Last commit created an importation issue. It was solved.

--- instelec/__init__.py
+++ instelec/__init__.py
@@ -6,6 +6,4 @@ should interact.
 from .settings import ureg, VOLTAGE_FF, VOLTAGE_FN
 from .power_triangle import PowerTriangle
 from .engines import Engine, EngineGroup, simultaneity_factor
-from .condutor_dimensioning import (
-    CupperPVC, CupperEPR, CupperXLPE, Grouping, TemperatureCorrectionAmbient
-)
+from .condutor_dimensioning import CupperPVC, CupperEPR, CupperXLPE
